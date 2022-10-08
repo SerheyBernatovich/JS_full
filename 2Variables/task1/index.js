7b602dd0b43a703b217044dd39007599595e0d59
SerheyBernatovich <sec1982@ukr.net>
2022-10-08 15:19:50 +0300

    new

---
+++ 2Variables/task1/index.js
@@ -0,0 +1,17 @@
+/*
+const User Name = 'John';
+const 1greeting = "I'm John";
+const ho%b@by = 'Walking';
+const FavouriteDESSERT = 'cake';
+const super&Number = 17;
+*/
+// пробел и начин с большой букви
+const userName = 'John'; 
+// начинается с цифры
+const greeting = "I'm John";
+// содержит спец символ
+const hobby = 'Walking';
+// начин с больш буквы и содержит следующее слово с больших букв
+const favouriteDessert = 'cake';
+// содержит спецсимвол
+const superNumber = 17;
