293482cd6216806e7ba2f6e9e2ceedeb6161b739
SerheyBernatovich <sec1982@ukr.net>
2023-02-13 13:46:09 +0200

    add

---
+++ 14Lexical_environment_scope/task2/index.js
@@ -0,0 +1,16 @@
+export default function createMessenger() {
+  let message = 'Just learn it';
+  let sender = 'Gromcode';
+
+  const sendMessage = name => {
+    console.log(`${name}, ${message}! Your ${sender}`);
+  };
+
+  const setMessage = text => {
+    message = text;
+  };
+  const setSender = senderName => {
+    sender = senderName;
+  };
+  return { sendMessage, setMessage, setSender };
+}
