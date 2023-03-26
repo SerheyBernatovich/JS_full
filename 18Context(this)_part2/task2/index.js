a728a8d3f424dce5ebbe71548232ee422eee3feb
SerheyBernatovich <sec1982@ukr.net>
2023-03-26 09:54:00 +0300

    add

---
+++ 18Context(this)_part2/task2/index.js
@@ -0,0 +1,11 @@
+export const wallet = {
+  transactions: [1, 3, 5, 8],
+  getMax() {
+    return Math.max(...this.transactions);
+  },
+  getMin() {
+    return Math.min(...this.transactions);
+  },
+};
+console.log(wallet.getMax());
+console.log(wallet.getMin());
