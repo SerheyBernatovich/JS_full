da244fa4e030101a7508e0db180fd63ec70e5fc6
SerheyBernatovich <sec1982@ukr.net>
2023-04-03 10:53:46 +0300

    add

---
+++ 19Prototypes.Inheritance/task1/index.js
@@ -0,0 +1,26 @@
+export const vehicle = {
+  name: 'Argo',
+  move() {
+    console.log(`${this.name} is moving`);
+  },
+  stop() {
+    console.log(`${this.name} stopped`);
+  },
+};
+
+export const ship = {
+  name: 'Argo',
+
+  startMachine() {
+    console.log(`${this.name} lifting anchor up`);
+    this.move();
+  },
+  stopMachine() {
+    console.log(`${this.name} lifting anchor down`);
+    this.stop();
+  },
+};
+Object.setPrototypeOf(ship, vehicle);
+
+ship.move();
+ship.stop();
