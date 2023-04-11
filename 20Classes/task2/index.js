361f5521ea7902b1a8937b1cf35811d540adfcd1
SerheyBernatovich <sec1982@ukr.net>
2023-04-11 08:05:06 +0300

    add

---
+++ 20Classes/task2/index.js
@@ -0,0 +1,34 @@
+/* eslint-disable max-classes-per-file */
+export class Vehicle {
+  constructor(name, numberOfWheels) {
+    this.name = name;
+    this.numberOfWheels = numberOfWheels;
+  }
+
+  move() {
+    console.log(`${this.name} is moving`);
+  }
+
+  stop() {
+    console.log(`${this.name} stopped`);
+  }
+}
+export class Ship extends Vehicle {
+  constructor(name, numberOfWheels, maxSpeed) {
+    super(name, numberOfWheels);
+    this.maxSpeed = maxSpeed;
+  }
+
+  move() {
+    console.log(`${this.name} lifting anchor up`);
+    super.move();
+  }
+
+  stop() {
+    super.stop();
+    console.log(`${this.name} lifting anchor down`);
+  }
+}
+const ship1 = new Ship('Argo', false, 17);
+ship1.move();
+console.log(ship1);
