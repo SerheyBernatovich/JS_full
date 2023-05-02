1e505252146680277ef177507705f42fe6ea1fba
SerheyBernatovich <sec1982@ukr.net>
2023-05-02 07:47:12 +0300

    add

---
+++ 21DOM/task2/index.js
@@ -0,0 +1,13 @@
+export function getTitleElement() {
+  const result = document.querySelector('.title');
+  console.dir(result);
+  return result;
+}
+
+export function getInputElement() {
+  const result = document.querySelector("[type = 'text']");
+  console.dir(result);
+  return result;
+}
+getTitleElement();
+getInputElement();
