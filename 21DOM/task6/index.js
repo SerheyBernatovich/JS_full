61650c989e4688cd5002b586d65998577f6c7d61
SerheyBernatovich <sec1982@ukr.net>
2023-05-20 13:19:08 +0300

    add

---
+++ 21DOM/task6/index.js
@@ -0,0 +1,5 @@
+export function setButton(text) {
+  document.querySelector('body').innerHTML = `<button>${text}</button>`;
+  // document.querySelector("body").textContent = `<button>${text}</button>`;
+}
+setButton('tee');
